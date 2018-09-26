9574ec7367296e547010ce96738dea41ee9c7a97
vinyex <irvinepramudya@gmail.com>
2018-09-26 14:41:46 +0700

    CRUD Lumen REST-API

---
+++ app/Http/Controllers/todoController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\ModelTodo;
+use Illuminate\Http\Request;
+
+
+class todoController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        $data = ModelTodo::all();
+        return response($data);
+    }
+
+    public function show($id){
+        $data = ModelTodo::where('id',$id)->get();
+        return response ($data);
+    }
+
+    public function store (Request $request){
+        $data = new ModelTodo();
+        $data->activity = $request->input('activity');
+        $data->description = $request->input('description');
+        $data->save();
+
+        return response('Berhasil Tambah Data');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = ModelTodo::where('id',$id)->first();
+        $data->activity = $request->input('activity');
+        $data->description = $request->input('description');
+        $data->save();
+
+        return response('Berhasil Merubah Data');
+    }
+
+    public function destroy($id)
+    {
+        $data = ModelTodo::where('id',$id)->first();
+        $data->delete();
+
+        return response('Berhasil Menghapus Data');
+    }
+
+    //
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
